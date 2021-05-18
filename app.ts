f1dc81e4ce65adfab98192b1bfb71b9e5d0a040b
Pablo Martínez <martipev2@hotmail.com>
2021-05-17 21:34:34 -0500

    chore(server): views eliminadas

--- app.ts
+++ app.ts
@@ -71,16 +71,10 @@ app.use(session(sess));
 
 // JSON parsing
 app.use(express.json());
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.urlencoded({ extended: false }));
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Routers
 app.use('/ping', pingRouter);
