891dc2edc0990973a0bf98600fa97072f8bfe183
Pablo Martínez <martipev2@hotmail.com>
2021-05-17 23:39:18 -0500

    build(server): agregada bandera de pruebas [temp]

--- routes/distritos/distritos.ts
+++ routes/distritos/distritos.ts
@@ -13,12 +13,13 @@ dotenv.config();
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const isProduction = process.env.NODE_ENV === 'production';
+const isTesting = process.env.TESTING === 'test';
 const distritosRouter = express.Router();
 const pgp = pgpInit();
 const db = pgp(isProduction ? process.env.DATABASE_URL as string : connectionString);
 const accessList = ['https://latitud312.com/', 'https://latitud312-quiz.vercel.app/'];
 const corsOptions = {
-  origin: isProduction
+  origin: isProduction && isTesting
     ? function (origin, callback) {
       if (accessList.indexOf(origin) !== -1) {
         callback(null, true);
